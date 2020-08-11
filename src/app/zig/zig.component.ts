ad6f03c7e1ac678783cf077228055fa9c6004a6d
Allan Kanyemba <allankanyemba@gmail.com>
2020-08-12 00:11:58 +0200

    homepage updates

--- src/app/zig/zig.component.ts
+++ src/app/zig/zig.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-zig',
@@ -8,16 +9,19 @@ import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-i
 })
 export class ZigComponent implements OnInit {
 
+  // Font Awesome Icons
   faFacebook = faFacebook;
   faYoutube = faYoutube;
   faTwitter = faTwitter;
+  faPlay = faPlay;
+  // Accordion steps declaration
   step = 0;
 
   constructor( ) { }
 
   ngOnInit(): any { }
 
-  setStep(index: number): void {
+  setStep(index: number): any {
     this.step = index;
   }
 
