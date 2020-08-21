57c69eb6fcc45d405f9675def2a50a88c12e11b0
Allan Kanyemba <allankanyemba@gmail.com>
2020-08-22 00:53:47 +0200

    Started Working on ZIGYN UI

--- src/app/zigyn/zigyn.component.ts
+++ src/app/zigyn/zigyn.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faTwitter, faFacebook, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faUser, faComments, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-zigyn',
@@ -6,6 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zigyn.component.css']
 })
 export class ZigynComponent implements OnInit {
+  // Get Font Awesome Icons
+  faTwitter = faTwitter;
+  faFacebook = faFacebook;
+  faYoutube = faYoutube;
+  faUser = faUser;
+  faCalendar = faCalendar;
+  faComments = faComments;
+  // Latest News Variables
+  author = 'John';
+  month: number = new Date().getMonth();
+  day: number = new Date().getDay();
+  year: number = new Date().getFullYear();
+  datePublished = this.month.toString() + ' / ' + this.day.toString() + ' / ' + this.year.toString();
+  // OwlCorousel For Leadership Profiles
+  leadershipOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['prev', 'next'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: false
+  };
 
   constructor() { }
 
